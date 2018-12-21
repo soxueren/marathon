8677142f10cc26ccac3680d3051147130b154ae7
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2018-12-21 15:46:37 +0100

    Added an experimental `/start` endpoint to save a template and launch instances

--- src/main/scala/mesosphere/marathon/api/v3/TemplatesResource.scala
+++ src/main/scala/mesosphere/marathon/api/v3/TemplatesResource.scala
@@ -15,11 +15,12 @@ import mesosphere.marathon.api.v2.Validation._
 import mesosphere.marathon.api.v2.json.Formats._
 import mesosphere.marathon.api.v2.{AppHelpers, AppNormalization}
 import mesosphere.marathon.api.{AuthResource, RestResource}
+import mesosphere.marathon.core.launchqueue.LaunchQueue
 import mesosphere.marathon.core.plugin.PluginManager
 import mesosphere.marathon.experimental.repository.TemplateRepository
 import mesosphere.marathon.plugin.auth._
 import mesosphere.marathon.raml.Raml
-import mesosphere.marathon.state._
+import mesosphere.marathon.state.{AppDefinition, PathId}
 import org.glassfish.jersey.server.ManagedAsync
 import play.api.libs.json.{JsString, Json}
 
@@ -34,6 +35,7 @@ class TemplatesResource @Inject() (
     templateRepository: TemplateRepository,
     eventBus: EventStream,
     val config: MarathonConf,
+    launchQueue: LaunchQueue,
     pluginManager: PluginManager)(
     implicit
     val authenticator: Authenticator,
@@ -53,9 +55,23 @@ class TemplatesResource @Inject() (
   private implicit val validateAndNormalizeApp: Normalization[raml.App] =
     appNormalization(config.availableFeatures, normalizationConfig)(AppNormalization.withCanonizedIds())
 
+  // TODO(ad): this whole API idea of:
+  //           - GET v3/templates/{id}
+  //           - GET v3/templates/{id}/versions
+  //           - GET v3/templates/{id}/latest
+  //           is IMHO not very comfortable. I'd rather go towards "browsing tree" approach e.g.
+  //
+  // TODO(ad): new API proposal:
+  //           - GET v3/templates/ - returns *all* children of `/` with type (folder/service) and url
+  //           - GET v3/templates/foo - returns children for `/foo`. If '/foo' is folder, return list of service urls,
+  //             otherwise, list of versions of `/foo`
+  //           - GET v3/templates/{id} - tree traversal where {id} has arbitrary nesting
+  //           - GET v3/templates/foo/s12d234d2 - content of template `/foo` in version `s12d234d2`
+  //
   // TODO(ad): add checking resource authorization for all calls
-  // TODO(ad): prevent users from modifying same pathId concurrently
+  // TODO(ad): prevent users from modifying same pathId concurrently? Do we need it?
   // TODO(ad): filter created pathIds for key words like `latest` etc
+  // TODO(ad): don't delete templates if there are instances referencing it
 
   @POST
   @ManagedAsync
@@ -193,6 +209,44 @@ class TemplatesResource @Inject() (
 
       Response
         .ok()
+        .build()
+    }
+  }
+
+  /**
+    * ====================== EXPERIMENTAL ======================
+    *
+    * All the code in this class should be considered experimental but this method especially. Here, we mix orchestration
+    * logic (starting instances) with template concerns (saving them). While, this kind of endpoint can be very handy, I
+    * don't think we should have it underneath `/templates`.
+    *
+    * @param body
+    * @param req
+    * @param asyncResponse
+    */
+  @POST
+  @ManagedAsync
+  @Path("start")
+  def start(
+    body: Array[Byte],
+    @Context req: HttpServletRequest,
+    @Suspended asyncResponse: AsyncResponse): Unit = sendResponse(asyncResponse) {
+    async {
+      implicit val identity = await(authenticatedAsync(req))
+
+      val rawApp = Raml.fromRaml(Json.parse(body).as[raml.App].normalize)
+      val app = validateOrThrow(rawApp)
+      val version = await(templateRepository.create(app))
+
+      logger.info(s"Scheduling ${app.instances} instance(s) for the ${app.id}")
+      // Ideally, LaunchQueue is an implementation detail and an orchestrator only knows
+      // the instance tracker: instanceTracker.schedule(Instance.scheduled(app))
+      // We await the result here since that guaranties that the instance is persisted.
+      await(launchQueue.add(app, count = app.instances))
+
+      Response
+        .created(new URI(app.id.toString))
+        .entity(jsonObjString("version" -> JsString(version)))
         .build()
     }
   }
